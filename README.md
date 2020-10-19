57bfc8e6ed9e581bd2348ff81a1f68db88f2e120
Sabatol <u.sacrez@gmail.com>
2020-10-19 23:30:41 +0200

    Update

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-Vous êtes en présence d'un readme généré automatiquement !
+Ceci est une application qui permet d'écrire, de consulter et d'éditer des potins sur le sujet de votre choix !
 
-...Bon. Si vous êtes là, c'est forcément que j'ai oublié quelque chose...
+Pour faire fonctionner le programme :
+
+1°/ Exécutez 'shotgun -p 4567' dans le dossier source de l'application
+
+2°/ Allez à l'URL: http://localhost:4567/
+
+3°/ Consultez, écrivez et éditez vos propres potins !
+
+-----------------------------------------------------------------------------
+
+Travail effectué jusqu'au 2.7 : 'éditer un potin'
+Manque : 'commentaires de potins'
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,33 +1,36 @@
 require 'gossip'
 
 class ApplicationController < Sinatra::Base
-  # Opération get sur la page '/' qui prend la requête du visiteur et l'envoie vers l'index
+  # Opération get sur la page '/' qui prend la requête du visiteur et l'envoie vers index.erb
   get '/' do
     erb :index, locals: {gossips: Gossip.all}
   end
 
-  # Opération get sur la page '/gossips/new/' qui prend la requête du visiteur et l'envoie vers
+  # Opération get sur la page '/gossips/new/' qui prend la requête du visiteur et l'envoie vers new_gossip.erb
   get '/gossips/new/' do
     erb :new_gossip
   end
 
-  # On créé une nouvelle gossip via le formulaire de la page new_gossip !
+  # On créé une nouvelle gossip via le formulaire (post) de la page new_gossip !
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
-    puts "Il reste un truc là dedans ? : #{params["gossip_author"]} Oui !"
-    puts "Et là dedans ? : #{params["gossip_content"]} Oui aussi !"
-    puts "Oh mais enfait je suis dans le terminal là ! Personne peut me voir ?!"
+    puts "Il reste un truc là dedans ? : '#{params["gossip_author"]}' Oui !"
+    puts "Et là dedans ? : '#{params["gossip_content"]}' Oui aussi !"
+    puts "Oh mais enfait je suis dans le terminal là ! Personne ne peut me voir !"
     redirect '/'
   end
 
+  # Opération get sur la page '/gossips/:id/' (chemin dynamique) qui prend la requête du visiteur et l'envoie vers show.erb
   get '/gossips/:id' do
     erb :show, locals: {gossip: Gossip.find(params["id"])}
   end
 
+  # Opération get sur la page '/gossips/:id/edit/' qui prend la requête du visiteur et l'envoie vers edit.erb (formulaire pour récupérer un post)
   get '/gossips/:id/edit/' do
     erb :edit, locals: {id: params["id"]}
   end
 
+  # Exécute la fonction update pour modifier un gossip via les paramètres entrés par l'user (post) !
   post '/gossips/:id/edit/' do
     Gossip.update(params["id"], params["gossip_author"], params["gossip_content"])
     redirect '/'
